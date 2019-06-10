048939520555eaa661b719ea546596bbcad245c9
paetalung <paetalung@gmail.com>
2019-06-10 19:29:30 +0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace demo11.class_n_inheritance
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
